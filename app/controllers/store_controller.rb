fc8e4828d4bdc70b89eed6b8b807f60b7deb42d5
Kenta Iwanaga <kenta@is-pro.net>
2015-11-16 04:59:44 +0000

    カートの仕上げ

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,5 +2,16 @@ class StoreController < ApplicationController
   
   def index
     @products = Product.order(:title)
+
+    puts "-------------------"
+    puts session[:counter]
+    puts "-------------------"
+
+    if session[:counter].nil?
+      session[:counter] = 1
+    else
+      session[:counter] += 1
+    end
+    @counter = session[:counter]
     end
 end
