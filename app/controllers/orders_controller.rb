fb47c8525f7d8444c38ba6cecdbf864baba302f0
Kenta Iwanaga <kenta@is-pro.net>
2015-11-20 00:38:15 +0000

    テスト終了

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -42,6 +42,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver_now
         format.html { redirect_to store_url, notice: 'ご注文ありがとうございます' }
         format.json { render json: @order, status: :created, location: @order }
       else
---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "Kenta Iwanaga <info@depot.com>"
+  layout 'mailer'
+end
