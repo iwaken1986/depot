edca9e00a00bab412679ffc2d9ffe2f3a8a61459
Kenta Iwanaga <kenta@is-pro.net>
2015-11-24 06:39:32 +0000

    国際化

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,11 +1,18 @@
 class StoreController < ApplicationController
   skip_before_action :authorize
   def index
+
+    if params[:set_locale]
+      redirect_to store_path(locale: params[:set_locale])
+    else
+      @products = Product.order(:title)
+      @cart = current_cart
+
     @products = Product.order(:title)
 
-    puts "-------------------"
-    puts session[:counter]
-    puts "-------------------"
+    #puts "-------------------"
+    #puts session[:counter]
+    #puts "-------------------"
 
     if session[:counter].nil?
       session[:counter] = 1
@@ -16,4 +23,5 @@ class StoreController < ApplicationController
     @counter = session[:counter]
     @cart = current_cart
     end
+  end
 end
